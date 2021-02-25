b6635ea22eb5530207eb1317ba2083a0534c57f1
Katy Yaryvanovich <k.yaryvanovich.git@gmail.com>
2021-02-25 18:59:04 +0300

    add modules and code for upload and redactedit images

---
+++ js/upload-img.js
@@ -0,0 +1,54 @@
+import { isEscEvent, isEnterEvent } from './util.js'
+import { scaleValue, scaleDefolt, uploadPreview } from './scale-upload-img.js'
+import {slider} from './effect-upload-img.js';
+
+
+const uploadImg = document.querySelector('#upload-file');
+const uploadOverlay = document.querySelector('.img-upload__overlay');
+const bodyModalOpen = document.querySelector('body');
+const uploadCancel = document.querySelector('#upload-cancel');
+
+
+const openUploadImg = () => {
+  uploadOverlay.classList.remove('hidden');
+  bodyModalOpen.classList.add('modal-open');
+  scaleValue.value = `${scaleDefolt} %`;
+  uploadPreview.style.transform = 'scale(1)';
+  uploadPreview.style.filter = 'none';
+  slider.classList.add('visually-hidden');
+  document.addEventListener('keydown', onPopupEscKeydown);
+  uploadOverlay.addEventListener('keydown', onPopupEnterKeydown);
+}
+
+uploadImg.addEventListener('change', () => {
+  openUploadImg();
+});
+
+
+const closeUploadImg = () => {
+  uploadOverlay.classList.add('hidden');
+  bodyModalOpen.classList.remove('modal-open');
+  uploadImg.value = '';
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  uploadOverlay.removeEventListener('keydown', onPopupEnterKeydown);
+}
+
+uploadCancel.addEventListener('click', () => {
+  closeUploadImg();
+});
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscEvent(evt)) {
+    evt.preventDefault();
+    closeUploadImg();
+  }
+};
+
+const onPopupEnterKeydown = (evt) => {
+  if (isEnterEvent(evt)) {
+    openUploadImg();
+  }
+};
+
+
+
