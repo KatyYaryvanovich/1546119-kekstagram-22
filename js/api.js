8c734477b03f36013d501f310ccc2b363317480d
Katy Yaryvanovich <k.yaryvanovich.git@gmail.com>
2021-03-06 15:01:19 +0300

    fixed server connection

--- js/api.js
+++ js/api.js
@@ -1,33 +1,37 @@
-const getData = (onSuccess, onFail) => {
-  fetch('https://22.javascript.pages.academy/kekstagram/data')
-    .then((response) => response.json())
-    .then((pictures) => {
-      // getPreview(pictures);
-      onSuccess(pictures);
-    })
-    .catch(() => {
-      onFail()
-    });
+const getData = async (onSuccess, onFail) => {
+  let response;
+  try {
+    response = await fetch('https://22.javascript.pages.academy/kekstagram/data')
+    if (response.ok) {
+      const pictures = await response.json()
+      return onSuccess(pictures)
+    }
+  } catch (error) {
+    return onFail()
+  }
 }
 
-const sendData = (onSuccess, onFail, body) => {
-  fetch(
-    'https://22.javascript.pages.academy/kekstagram',
-    {
-      method: 'POST',
-      body,
-    },
-  )
-    .then((response) => {
-      if (response.ok) {
-        onSuccess();
-      } else {
-        throw new Error();
-      }
-    })
-    .catch(() => {
-      onFail();
-    });
-};
+const sendData = async (onSuccess, onFail, pictures) => {
+  let response;
+  try {
+    response = await fetch(
+      'https://22.javascript.pages.academy/kekstagram',
+      {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+        body: JSON.stringify(pictures),
+      },
+    );
+  } catch (error) {
+    return onFail()
+  }
+
+  if (response.ok) {
+    onSuccess();
+  } else {
+    throw new Error();
+  }
+}
+
 
 export { getData, sendData };
--- js/message-for-user.js
+++ js/message-for-user.js
@@ -1,22 +1,7 @@
 import { sendData } from './api.js'
 import { showMessage } from './util.js'
 
-const showAlert = (message) => {
-  const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = 100;
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = 0;
-  alertContainer.style.top = 0;
-  alertContainer.style.right = 0;
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
 
-  alertContainer.textContent = message;
-
-  document.body.append(alertContainer);
-}
 
 
 const uploadForm = document.querySelector('.img-upload__form');
@@ -25,17 +10,15 @@ const setUserFormSubmit = (onSuccess, onFail) => {
   uploadForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
-    sendData(() => {
-      showMessage();
-      showAlert('Успешно');
-      onSuccess();
-    }),
-
-    sendData(() => {
-      showMessage();
-      showAlert('Упс...Не удалось загрузить. Попробуйте повторить через пару секунд');
-      onFail();
-    }),
+    sendData(
+      () => {
+        showMessage('success', 'Успешно');
+        onSuccess();
+      },
+      () => {
+        showMessage('error', 'Упс...Не удалось загрузить. Попробуйте повторить через пару секунд');
+        onFail();
+      }),
 
     new FormData(evt.target)
   });
